2d89240f6fb159080d89f0aa9e16a923ad6b1127
Moxie Marlinspike <moxie@thoughtcrime.org>
2013-07-12 08:38:21 -0700

    Add copyright headers and author tags.

--- src/org/whispersystems/whisperpush/service/SmsListener.java
+++ src/org/whispersystems/whisperpush/service/SmsListener.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.whispersystems.whisperpush.service;
 
 import android.content.BroadcastReceiver;
@@ -13,6 +28,8 @@ import org.whispersystems.whisperpush.util.WhisperPreferences;
  * A BroadcastReceiver that listens for incoming SMS events.  If the incoming SMS
  * is a registration challenge, it'll abort the broadcast and send a notification
  * to the RegistrationService.
+ *
+ * @author Moxie Marlinspike
  */
 public class SmsListener extends BroadcastReceiver {
 
