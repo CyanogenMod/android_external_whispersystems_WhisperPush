e1ab429063447ee26a8ea881db7007eda6c598d7
Chris Soyars <ctso@ctso.me>
2013-11-25 18:56:36 -0800

    Update GCM Sender ID

--- src/org/whispersystems/whisperpush/Release.java
+++ src/org/whispersystems/whisperpush/Release.java
@@ -2,6 +2,6 @@ package org.whispersystems.whisperpush;
 
 public class Release {
 
-  public static final String GCM_SENDER_ID = "312334754206";
+  public static final String GCM_SENDER_ID = "361461532168";
   public static final String PUSH_URL = "https://whisperpush.cyanogenmod.org";
 }
