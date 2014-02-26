1813f992ec64b2e29049e13bbf3db815a35a0539
Michael Gernoth <michael@gernoth.net>
2014-02-26 01:05:25 +0100

    WhisperPush: Make it possible to unregister
    An active registration in CM WhisperPush conflicts with a registration
    for the same device on the TextSecure-servers. This change allows to
    unregister from the CM servers.
    
    Change-Id: I632012e7bb29e6d13b7d19fbe3875cd47688c317

--- src/org/whispersystems/whisperpush/ui/RegistrationCompletedActivity.java
+++ src/org/whispersystems/whisperpush/ui/RegistrationCompletedActivity.java
@@ -18,11 +18,15 @@ package org.whispersystems.whisperpush.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 
 import org.whispersystems.whisperpush.R;
 import org.whispersystems.whisperpush.util.WhisperPreferences;
+import org.whispersystems.textsecure.push.PushServiceSocket;
+import org.whispersystems.whisperpush.util.PushServiceSocketFactory;
 
 public class RegistrationCompletedActivity extends Activity implements View.OnClickListener {
     @Override
@@ -31,12 +35,35 @@ public class RegistrationCompletedActivity extends Activity implements View.OnCl
         setContentView(R.layout.registration_completed_activity);
 
         findViewById(R.id.registerAgainButton).setOnClickListener(this);
+        findViewById(R.id.unregisterButton).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        WhisperPreferences.setRegistered(this, false);
-        startActivity(new Intent(this, RegistrationActivity.class));
-        finish();
+        if (v == findViewById(R.id.registerAgainButton)) {
+            WhisperPreferences.setRegistered(this, false);
+            startActivity(new Intent(this, RegistrationActivity.class));
+            finish();
+        } else if (v == findViewById(R.id.unregisterButton)) {
+            new AsyncTask<Void, Void, Void>() {
+                @Override
+                protected Void doInBackground(Void... param) {
+                    Boolean success = Boolean.TRUE;
+
+                    PushServiceSocket socket = PushServiceSocketFactory.create(getApplicationContext());
+                    try {
+                        socket.unregisterGcmId();
+                    } catch (java.io.IOException e) {
+                        return null;
+                    }
+                    WhisperPreferences.setRegistered(getApplicationContext(), false);
+                    return null;
+                }
+                @Override
+                protected void onPostExecute(Void result) {
+                    finish();
+                }
+            }.execute();
+        }
     }
 }
