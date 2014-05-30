8652300c8b8f5443eec14f8ed0298e30ce651f6f
Roman Birg <roman@cyngn.com>
2014-05-30 13:17:56 -0700

    WhisperPush: remove Play Services define
    Change-Id: I681b9baa5c415565723ed92ef0de57034ae2678d
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- Android.mk
+++ Android.mk
@@ -9,12 +9,11 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := WhisperPush
 LOCAL_CERTIFICATE := platform
+LOCAL_PROGUARD_ENABLED := disabled
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --extra-packages com.google.android.gms -S $(LOCAL_PATH)/../../google/google_play_services/libproject/google-play-services_lib/res --auto-add-overlay
 
 LOCAL_STATIC_JAVA_LIBRARIES := play TextSecureLibrary
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := \
-    play:../../google/google_play_services/libproject/google-play-services_lib/libs/google-play-services.jar
 
 include $(BUILD_PACKAGE)
