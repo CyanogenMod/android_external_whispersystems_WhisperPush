1e96c6e97f7d1c84efd32dfb09d213599b390bca
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-06-15 14:22:33 -0700

    WhisperPush: sign with app key when building for production
    Sign WP with app key when building for production, and with
    platform key otherwise.
    
    Change-Id: I0d44d5858c1a637ad08f37ce114214c5504e36b6

--- Android.mk
+++ Android.mk
@@ -8,12 +8,17 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_PACKAGE_NAME := WhisperPush
-LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
+
+ifneq ($(DEFAULT_SYSTEM_DEV_CERTIFICATE),build/target/product/security/testkey)
+  LOCAL_CERTIFICATE := cyngn-app
+else
+  LOCAL_CERTIFICATE := platform
+endif
 
 LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
 LOCAL_AAPT_FLAGS := --extra-packages com.google.android.gms -S $(LOCAL_PATH)/../../google/google_play_services/libproject/google-play-services_lib/res --auto-add-overlay
 
-LOCAL_STATIC_JAVA_LIBRARIES := play axolotl-android textsecure-android 
+LOCAL_STATIC_JAVA_LIBRARIES := play axolotl-android textsecure-android
 
 include $(BUILD_PACKAGE)
