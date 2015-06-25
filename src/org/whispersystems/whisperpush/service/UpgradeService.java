57ac48041205ec517b182ac309abfd5dc37781de
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-06-25 16:05:47 -0700

    WhisperPush: service for WPv2 update to interrogate/disable v1
    WhisperPush vs will be delievered with a different package name.
    It will uses this remote service to determine if the old WP is
    actually registered to determine if the user should be prompted
    to register, and then it will disable the old package so it no
    longer intercepts messages.
    
    Change-Id: I60bd0ea90ec5a9f8e6812e41553b1f5d010eed80

---
+++ src/org/whispersystems/whisperpush/service/UpgradeService.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (C) 2013 The CyanogenMod Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.whispersystems.whisperpush.service;
+
+import org.whispersystems.whisperpush.util.WhisperPreferences;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
+import android.os.RemoteException;
+
+public class UpgradeService extends Service {
+    volatile boolean mDisableOnUnbind = false;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        boolean result = super.onUnbind(intent);
+        if (mDisableOnUnbind) {
+            // goodbye, cruel world...
+            PackageManager pm = getPackageManager();
+            pm.setApplicationEnabledSetting(getPackageName(),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+        }
+        return result;
+    }
+
+    private final IUpgrade.Stub mBinder = new IUpgrade.Stub() {
+        @Override
+        public boolean isRegistered() throws RemoteException {
+            return WhisperPreferences.isRegistered(UpgradeService.this);
+        }
+
+        @Override
+        public String getLocalNumber() throws RemoteException {
+            return WhisperPreferences.getLocalNumber(UpgradeService.this);
+        }
+
+        @Override
+        public void disableOnUnbind() throws RemoteException {
+            mDisableOnUnbind = true;
+        }
+    };
+}
