0fd9ecb84ef7222c2af3f899ce3f7ced1f743753
Wilhelm Fitzpatrick <rafial@cyngn.com>
2015-06-26 14:10:41 -0700

    WhisperPush: add weekly active users metric
    To help the business organization make the case for future
    investment in WhisperPush, we need to be able to assess how many
    of those who are registered for WhisperPush are actually actively
    using it to exchange messages.
    
    This patch introduces a "weekly active users" metric. Once per
    week, an event will be sent if the user had any message traffic
    via WP (but not the quantity). This should be imprecise enough
    to maintain privacy, while still giving us insight into how many
    are depending on the service over time.
    
    If the user has opted out of metric collection, then this metric
    will neither be collected nor sent.
    
    Change-Id: Id32a9811a4bf7eda24ac7c5a10ed479e5dc291cd

---
+++ src/org/whispersystems/whisperpush/util/StatsUtils.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2015 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.whispersystems.whisperpush.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.UserHandle;
+import android.provider.Settings;
+
+public class StatsUtils {
+    private static final String STATS_PACKAGE = "com.cyngn.stats";
+    private static final String ANALYTIC_INTENT = "com.cyngn.stats.action.SEND_ANALYTIC_EVENT";
+    private static final String ANALYTIC_PERMISSION = "com.cyngn.stats.SEND_ANALYTICS";
+    private static final String TRACKING_ID = "tracking_id";
+
+    public static boolean isStatsActive(Context context) {
+        return isStatsPackageInstalled(context) && isStatsCollectionEnabled(context);
+    }
+
+    public static boolean isStatsCollectionEnabled(Context context) {
+        return Settings.Secure.getInt(context.getContentResolver(),
+                Settings.Secure.STATS_COLLECTION, 1) != 0;
+    }
+
+    public static boolean isStatsPackageInstalled(Context context) {
+        try {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(STATS_PACKAGE, 0);
+            return pi.applicationInfo.enabled
+                    && ((pi.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
+
+    public static void sendWeeklyActiveUserEvent(Context context) {
+        if (!isStatsActive(context)) { return; }
+
+        // Create new intent
+        Intent intent = new Intent();
+        intent.setAction(ANALYTIC_INTENT);
+
+        // add tracking id
+        intent.putExtra(TRACKING_ID, context.getPackageName());
+        // metric
+        intent.putExtra("category", "usage");
+        intent.putExtra("action", "active");
+        intent.putExtra("label", "weekly");
+        if(WhisperPreferences.getWasActive(context)) {
+            intent.putExtra("value", "active");
+        } else {
+            intent.putExtra("value", "inactive");
+        }
+
+        // reset activity marker
+        WhisperPreferences.setWasActive(context, false);
+
+        // broadcast for internal package
+        context.sendBroadcastAsUser(intent,
+                new UserHandle(UserHandle.USER_CURRENT), ANALYTIC_PERMISSION);
+    }
+}
