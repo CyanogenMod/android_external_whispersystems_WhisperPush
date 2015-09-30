be26036fff5b0838e2bb45862c01718866d8fdc7
Yvonne Wong <ywong@cyngn.com>
2015-09-30 03:36:57 -0700

    WIP - Switch WhisperPush to use CMSettings
    Change-Id: I2b07295f97fc555b14331f4727bbc0df6ec4655f

--- src/org/whispersystems/whisperpush/util/StatsUtils.java
+++ src/org/whispersystems/whisperpush/util/StatsUtils.java
@@ -22,8 +22,9 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.UserHandle;
-import android.provider.Settings;
 import android.util.Log;
+
+import cyanogenmod.providers.CMSettings;
 
 public class StatsUtils {
     private static final String STATS_PACKAGE = "com.cyngn.stats";
@@ -36,8 +37,8 @@ public class StatsUtils {
     }
 
     public static boolean isStatsCollectionEnabled(Context context) {
-        return Settings.Secure.getInt(context.getContentResolver(),
-                Settings.Secure.STATS_COLLECTION, 1) != 0;
+        return CMSettings.Secure.getInt(context.getContentResolver(),
+                CMSettings.Secure.STATS_COLLECTION, 1) != 0;
     }
 
     public static boolean isStatsPackageInstalled(Context context) {
